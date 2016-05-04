47b2f0eeeef02323b86c4aa75748bdbd9b9af85b
Harihar Subramanyam <hahasubbu@gmail.com>
2016-05-04 10:18:58 -0400

    Update run script

---
+++ run_all.sh
@@ -0,0 +1,8 @@
+python run_tracker.py lobby71 tracker --out true
+python run_tracker.py lobby71 tracker --perspective true --out true
+python run_tracker.py lobby72 tracker --out true
+python run_tracker.py lobby72 tracker --perspective true --out true
+python run_tracker.py walker1 tracker --out true
+python run_tracker.py walker1 tracker --perspective true --out true
+python run_tracker.py walker2 tracker --out true
+python run_tracker.py walker2 tracker --perspective true --out true
--- run_tracker.py
+++ run_tracker.py
@@ -2,6 +2,7 @@
 
 import sys
 import os
+import argparse
 
 data = {
     "lobby71":
@@ -9,14 +10,16 @@ data = {
      1462273804.346465, 180,
      "~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Lobby7_1/tracker.json",
      "166 167 181 399 717 260 583 110",
-     "~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Lobby7_1/input_for_eric.csv"
+     "~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Lobby7_1/input_for_eric.csv",
+     "~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Lobby7_1/input_for_eric_perspective.csv"
      ),
     "lobby72":
     ("~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Lobby7_2/Lobby7_2.mov",
      1462274331.162998, 284,
      "~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Lobby7_2/tracker.json",
      "194 399 174 162 533 95 691 268",
-     "~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Lobby7_2/input_for_eric.csv"
+     "~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Lobby7_2/input_for_eric.csv",
+     "~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Lobby7_2/input_for_eric_perspective.csv"
      ),
     "walker1":
     ("~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Walker1/Walker1.mov",
@@ -25,35 +28,69 @@ data = {
      "~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Walker1/tracker.json",
      "2 401 259 16 485 22 636 455",
      "~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Walker1/input_for_eric.csv",
+     "~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Walker1/input_for_eric_perspective.csv",
      ),
     "walker2":
     ("~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Walker2/Walker2.mov",
      1462275918.126208, 326,
      "~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Walker2/tracker.json",
      "3 391 278 17 488 20 641 455",
-     "~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Walker2/input_for_eric.csv"
+     "~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Walker2/input_for_eric.csv",
+     "~/Dropbox/6.S062\ Final\ Project\ Videos/MITDataCollection2/Walker2/input_for_eric_perspective.csv"
      )
 }
 
-(videoPath, timestamp, duration, trackerFile, perspective,
- smoothedPathUrl) = data[sys.argv[1]]
+parser = argparse.ArgumentParser(
+    description='Run the tracker, plotter, or ground truth annotator.')
+parser.add_argument(
+    'input',
+    metavar='input',
+    type=str,
+    help=
+    "Name of input video (it should be one of the keys in the dictionary)")
+parser.add_argument('mode',
+                    metavar='mode',
+                    type=str,
+                    help="Should be tracker, plotter, or ground_truth")
+parser.add_argument('--perspective',
+                    metavar='perspective',
+                    type=bool,
+                    default=False,
+                    help='Whether a perspective transform should be applied')
+parser.add_argument('--out',
+                    metavar='out',
+                    type=bool,
+                    default=False,
+                    help='Whether an output file should be generated')
+args = parser.parse_args()
+
+(videoPath, timestamp, duration, trackerFile, perspective, smoothedPathUrl,
+ smoothedPathPerspectiveUrl) = data[args.input]
 
 maxDimension = 300
 
-# First run the object tracker.
-argumentTuple = [videoPath, trackerFile, maxDimension]
-formatString = "./start.sh -m tracker -i %s -s %s -d %s"
+if args.mode == "tracker":
+    # First run the object tracker.
+    argumentTuple = [videoPath, maxDimension]
+    formatString = "./start.sh -m tracker -i %s -d %s"
 
-if len(sys.argv) == 3:
-    argumentTuple.append(perspective)
-    formatString += " -p %s"
+    if args.perspective:
+        argumentTuple.append(perspective)
+        formatString += " -p %s"
 
-command = (formatString % tuple(argumentTuple))
-os.system(command)
+    if args.out:
+        argumentTuple.append(trackerFile)
+        formatString += " -s %s"
 
-# Smooth the trajectories.
-argumentTuple = [trackerFile, timestamp, duration, smoothedPathUrl]
-formatString = "python scripts/trajectory_smoother.py %s %s %s > %s"
-command = (formatString % tuple(argumentTuple))
+    command = (formatString % tuple(argumentTuple))
+    os.system(command)
 
-os.system(command)
+    # Smooth the trajectories.
+    smoothOutputFile = smoothedPathUrl
+    if args.perspective:
+        smoothOutputFile = smoothedPathPerspectiveUrl
+    if args.out:
+        argumentTuple = [trackerFile, timestamp, duration, smoothedPathUrl]
+        formatString = "python scripts/trajectory_smoother.py %s %s %s > %s"
+        command = (formatString % tuple(argumentTuple))
+        os.system(command)
